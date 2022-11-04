2ed3a19a2ad46b73c76150b65d44bef7ed3ca1e3
Rex <trex1213@gmail.com>
2022-11-03 20:00:35 -0400

    Ubuntu recovery. Organzied Files into Weeks numbers.

---
+++ Week02/shellclass/localusers/luser-demo01.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# This script displays various information to the screen.
+# Working on Week 2 Section 3 chapter 12
+
+# Display 'Hello'
+echo 'Hello'
+
+# Assign a value to a variable
+WORD='script'
+
+# Display that value using the variable.
+echo "$WORD"
+
+# Demostrate that single quotes cause variable to NOT get expanded.
+echo '$WORD'
+
+#combine the varibale with hard-coded text
+echo "This is a shell $WORD"
+
+# Display the contects of the variable using an alternative syntax.
+echo  "This is a shell ${WORD}"
+
+# Append text to teh variable.
+echo "${WORD}ing is fun!"
+
+# Show how NOT to append text to a variable.
+# This doesn't work.
+echo  "$WORDing is fun!"
+
+#Create  a new variable
+ENDING='ed'
+
+# Combine the two variables.
+echo "This is ${WORD}${ENDING}."
+
+# Change the value stored in teh ENDING variable. (Reassigment)
+ENDING='ing'
+echo "${WORD}${ENDING} is fun!"
+
+#Reassign value to ENDING
+ENDING='s'
+echo "You are going to write many ${WORD}${ENDING} in this class!"
