f48e01427909c1a206f3d792627a868e1406280d
Rex <trex1213@gmail.com>
2022-11-07 23:26:46 -0500

    chapter 20

---
+++ Week02/shellclass/localusers/luser-demo07.sh
@@ -0,0 +1,24 @@
+##!/bin/bash
+
+# Demonstrate the use of shift and while loops.
+
+# Display the first three parameters.
+echo "Parameter 1: ${1}"
+echo "Parameter 2: ${2}"
+echo "Parameter 3: ${3}"
+echo
+
+# Loop through all the positional parameters.
+while [[ "${#}" -gt 0 ]]
+do
+  echo "Number of parameters: ${#}"
+  echo "Parameter 1: ${1}"
+  echo "Parameter 2: ${2}"
+  echo "Parameter 3: ${3}"
+  echo
+  shift
+done
+
+# control C to break an infinite loop
+# Sleep to delay the next step/result.
+# gt is greater than 
