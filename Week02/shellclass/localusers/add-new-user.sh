2d038cc4efdd4e45d8856178d796c0e6e5eb0b4f
Rex <trex1213@gmail.com>
2022-11-08 20:22:54 -0500

    chapter 21 excercise.

--- Week02/shellclass/localusers/add-new-user.sh
+++ Week02/shellclass/localusers/add-new-user.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# This script creates a new user on the local system.
+#  You will need to sukpply a user name.
+#If you like you can also provide a comment for the account as an argument.
+# A password will be automactically  generated for the account. 
+# teh username, password, and host for the account will be displayed. 
+
+# Make sure the scritp is being execute with the superuser privileges.
+If [[ "$"]]
